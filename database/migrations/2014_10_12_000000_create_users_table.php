37f4ade978bdf851f58564e64ad284ea19d34ac7
ubeann <76271600+ubeann@users.noreply.github.com>
2023-05-23 18:29:59 +0700

    Create user model data

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -9,14 +9,14 @@ return new class extends Migration
     /**
      * Run the migrations.
      */
-    public function up(): void
-    {
+    public function up(): void {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('name')->min(3);
             $table->string('email')->unique();
+            $table->string('phone')->unique()->min(10)->max(15);
             $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
+            $table->string('password')->min(8)->max(32);
             $table->rememberToken();
             $table->timestamps();
         });
@@ -25,8 +25,7 @@ return new class extends Migration
     /**
      * Reverse the migrations.
      */
-    public function down(): void
-    {
+    public function down(): void {
         Schema::dropIfExists('users');
     }
 };
