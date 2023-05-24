8102ce8eb3cdc070ccad03dce6c3aed4a93a3c79
ubeann <76271600+ubeann@users.noreply.github.com>
2023-05-24 17:17:31 +0700

    Improve alert message

--- resources/views/components/alert.blade.php
+++ resources/views/components/alert.blade.php
@@ -1,4 +1,9 @@
 <div class="alert alert-{{ $type }} active">
-    <div class="message">{{ $message }}</div>
-    <span class="close-btn" id="btn-alert-close">&times;</span>
+    <div class="message">
+        <i class="fas fa-exclamation-circle"></i>
+        {{ $message }}
+    </div>
+    <span class="close-btn" id="btn-alert-close">
+        <i class="fas fa-times"></i>
+    </span>
 </div>
