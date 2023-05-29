b35f703c73ff1f51a9af5c0143f798b48e0a3d1e
ubeann <76271600+ubeann@users.noreply.github.com>
2023-05-29 10:54:51 +0700

    Create events model data

---
+++ app/Models/Event.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Event extends Model
+{
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'user_id',
+        'service',
+        'status',
+        'price',
+        'date',
+        'location',
+        'description',
+    ];
+
+    /**
+     * The attributes that should be cast.
+     *
+     * @var array<string, string>
+     */
+    protected $casts = [
+        'date' => 'date',
+    ];
+
+    /**
+     * Get the price with Rupiah format.
+     */
+    public function getPriceAttribute($value) {
+        return 'Rp' . number_format($value, 0, ',', '.');
+    }
+
+    /**
+     * Get the user that owns the event.
+     */
+    public function user() {
+        return $this->belongsTo(User::class);
+    }
+
+    /**
+     * Get the event's photos.
+     */
+    // public function photos() {
+    //     return $this->hasMany(Photo::class);
+    // }
+}
--- app/Models/User.php
+++ app/Models/User.php
@@ -43,4 +43,11 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
     ];
+
+    /**
+     * Get the events for the user.
+     */
+    public function events() {
+        return $this->hasMany(Event::class);
+    }
 }
