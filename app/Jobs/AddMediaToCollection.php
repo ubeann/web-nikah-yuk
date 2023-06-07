0c130c7249eb99bba05fe4acac3410affda6c4aa
ubeann <76271600+ubeann@users.noreply.github.com>
2023-06-07 15:20:05 +0700

    Create job for add media to collection

---
+++ app/Jobs/AddMediaToCollection.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Jobs;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldBeUnique;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+
+class AddMediaToCollection implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    protected Model $model;
+    protected string $collectionName;
+    protected string $filePath;
+
+    /**
+     * Create a new job instance.
+     */
+    public function __construct(Model $model, string $filePath, string $collectionName = 'default')
+    {
+        $this->model = $model;
+        $this->collectionName = $collectionName;
+        $this->filePath = $filePath;
+    }
+
+    /**
+     * Execute the job.
+     */
+    public function handle(): void
+    {
+        $this->model
+            ->addMedia($this->filePath)
+            ->toMediaCollection($this->collectionName);
+    }
+}
